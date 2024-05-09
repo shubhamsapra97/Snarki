544e325839af69947c6f0d2247a61bc69767953e
Shubham Sapra <shubham.sapra97@gmail.com>
2024-05-09 22:11:09 +0530

    Update dev_config.dart

--- lib/core/environments/dev_config.dart
+++ lib/core/environments/dev_config.dart
@@ -1,9 +1,9 @@
 import 'package:client/core/environments/base_config.dart';
 
 class DevConfig implements BaseConfig {
-  String get apiHost => "https://wz1u3d11cf.execute-api.us-east-1.amazonaws.com";
-  String get mapBoxToken => "pk.eyJ1Ijoic2h1YmhhbXNhcHJhOTciLCJhIjoiY2wybmdrdHRmMTZyeDNlcGttemRvaWN4YyJ9.7hTjPUh5aNteq9KqSto6PA";
+  String get apiHost => "";
+  String get mapBoxToken => "";
   bool get reportErrors => false;
   bool get trackEvents => false;
-  String get getRestaurantApiHost => "https://vceg4zcukh.execute-api.us-east-1.amazonaws.com";
+  String get getRestaurantApiHost => "";
 }
