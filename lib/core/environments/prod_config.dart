c3949d917e42900875445592871676b31d0d9481
Shubham Sapra <shubham.sapra97@gmail.com>
2024-05-09 22:11:30 +0530

    Update prod_config.dart

--- lib/core/environments/prod_config.dart
+++ lib/core/environments/prod_config.dart
@@ -1,9 +1,9 @@
 import 'package:client/core/environments/base_config.dart';
 
 class ProdConfig implements BaseConfig {
-  String get apiHost => "https://wz1u3d11cf.execute-api.us-east-1.amazonaws.com";
-  String get mapBoxToken => "pk.eyJ1Ijoic2h1YmhhbXNuYXJraSIsImEiOiJjbDJuaDB0OTgwMGFiM2dtcGhnc3lwMGU1In0.ReTVNO6W17QveKU_CiozrQ";
+  String get apiHost => "";
+  String get mapBoxToken => "";
   bool get reportErrors => false;
   bool get trackEvents => true;
-  String get getRestaurantApiHost => "https://vceg4zcukh.execute-api.us-east-1.amazonaws.com";
+  String get getRestaurantApiHost => "";
 }
